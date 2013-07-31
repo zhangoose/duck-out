1af3cca06d4d8804d0c12199290c65a8d7ed9f38
zhangoose <az177@eden.rutgers.edu>
2013-07-31 16:24:06 -0400

    created duck object & switched ghetto array of coordinates to array of duck objects

--- gameplay.js
+++ gameplay.js
@@ -7,15 +7,27 @@ function Duck(xCoord, yCoord){
 
 }//end of Duck object initializer
 
+function printDuck(duckArr){
+	//prints the attributes of the duck 
+	//for debugging
 
-function fillUpWithDucks(ctx, coords){
-	// fills up whatever coordinates specified in array 'coords' with duck drawings on screen
-	// does the drawing
+	var i;
+
+	for(i = 0; i < duckArr.length; i++){
+		console.log("duckArr["+i+"]: xCoord is " + duckArr[i].xCoord + " & yCoord is " + duckArr[i].yCoord + " & hits is " + duckArr[i].hits + " & display is " + duckArr[i].display);
+	}//end of for
+}//end of printDuck
+
+
+function fillUpWithDucks(ctx, duckArr){
+	// draws any duck in duckArr that has display set ias true
 	var i; // looper
 
-	for(i = 0 ; i < coords.length;  i=i+2){
-		console.log("x is " + coords[i] + "   | y is " + coords[i+1]);
-		duck(ctx,coords[i],coords[i+1]);	
+	for(i = 0 ; i < duckArr.length;  i++){
+//		console.log("x is " + coords[i] + "   | y is " + coords[i+1]);
+		if(duckArr[i].display == true){
+			duck(ctx,duckArr[i].xCoord,duckArr[i].yCoord);	
+		}//end of if should display or not
 	}//end of for 
 
 }//end of function fillUpWithDucks
@@ -25,12 +37,12 @@ function init_duckArray(c){
 	var ctx = c.getContext("2d");
 	var NUMDUCKS = 10;
 	var WIDTH  =c.width;
-	var duckBlocks = new Array(NUMDUCKS * 2); // x on odds, y on evens
-			// duckBlocks is the array of duck coords
+	var duckBlocks = new Array(NUMDUCKS); // ARRAY OF DUCK OBJECTS
 
 	var i; // looper for array
 	var offset = WIDTH / 6;
 	var x = 0; // x coord
+	var y = 0; // y coord
 
 	console.log("rawrawr length is " + duckBlocks.length);
 	console.log("rawrawr offset is " + offset);
@@ -39,35 +51,26 @@ function init_duckArray(c){
 		// x on odds
 		// y on evens
 
-		if(i % 2 == 0){
 			//  X
 			// going to be the same for either row
-			console.log("x");
-			if(i == 10){
+			if(i == duckBlocks.length /2){
 				x = 0; 
 			}//end of if
-			duckBlocks[i] = x; 
-			x = x + offset;
-			console.log("duckBlocks[" + i + "] = " + duckBlocks[i]);
-		}//end of if odd
 
-		else{
 			// Y
 			// different depending on which row
-			console.log("y");
-			if(i <= 10){
-				duckBlocks[i] = 0;
-				console.log("duckBlocks[" + i + "] = " + duckBlocks[i]);
+			if(i < duckBlocks.length /2){
+				y = 0;
 			}//first row
 			else{
-				duckBlocks[i] = offset;
-				console.log("duckBlocks[" + i + "] = " + duckBlocks[i]);
+				y  = offset;
 			}//second row
-
-		}//end of even
 		
+		duckBlocks[i] = new Duck(x,y); 
+		x = x + offset;
 	}//end of for looping through array to populate
-	
+	printDuck(duckBlocks);
+
 	return duckBlocks;
 
 }//end of function init_duckArray
